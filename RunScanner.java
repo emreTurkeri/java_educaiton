16ec7f39b172d2646f30aaa3bf6eded80d5e83e5
emreTurkeri <112562188+emreTurkeri@users.noreply.github.com>
2022-10-01 16:30:05 +0300

    Scanner

---
+++ RunScanner.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class RunScanner {
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter number ");
+
+        int number = 0;
+
+        number = input.nextInt();
+
+        System.out.println("Your number = " + number );
+
+    // Github = emreTurkeri
+    }
+}
