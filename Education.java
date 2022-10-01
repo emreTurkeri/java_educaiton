2339a720c7c4cd4ccb6c593cd12098c222e3b538
emreTurkeri <112562188+emreTurkeri@users.noreply.github.com>
2022-10-01 16:22:32 +0300

    Replace

--- Education.java
+++ Education.java
@@ -1,18 +1,36 @@
 import java.util.Scanner;
 
 public class Education {
-
     public static void main(String[] args){
 
-        Scanner input = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter number ");
+        System.out.println("Enter the number on the left");
 
-        int number = 0;
+        int numberOne = input.nextInt();
 
-        number = input.nextInt();
+        System.out.println("Enter the number on the right");
 
-        System.out.println("Your number = " + number );
+        int numberTwo = input.nextInt();
 
+        String mergeString = merge(numberOne,numberTwo).replace("&" , "UwU");
+        String printString = mergeString;
+        System.out.println(printString);
+
+        //OR
+        /* String mergeInt = merge(numberOne,numberTwo).replace("&","");
+        int PrintInt = Integer.parseInt(mergeInt);
+        System.out.println(printInt);
+        */
+
+    }
+
+    public static String merge(int left , int right){
+    //    String or String.valueOf()
+    String result = left + "&" + right;
+    return result;
+
+
+    // Github = emreTurkeri
     }
 }
