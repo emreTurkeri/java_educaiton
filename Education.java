ec940dba7cd7b49555d0d9282e44ba29b486876a
emreTurkeri <112562188+emreTurkeri@users.noreply.github.com>
2022-10-01 15:35:37 +0300

    Scanner

---
+++ Education.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Education {
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter number ");
+
+        int number = 0;
+
+        number = input.nextInt();
+
+        System.out.println("Your number = " + number );
+
+    }
+}
